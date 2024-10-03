7d74d3bd0315b108949f6268fcacdcdd74d5950a
fateme <fateme.mhmdi8221@gmail.com>
2024-10-03 11:02:39 +0330

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center gap-5 mt-20">
       <div className="PlayOnline mt-8">
-              <Link  title="پخش آنلاین" className="text-sm py-2.5 px-5 bg-orange-normal rounded-full"  href="/1">
+              <Link   className="text-sm py-2.5 px-5 bg-orange-normal rounded-full"  href="/1">
                فیلم یک
               </Link>
           </div>
       <div className="PlayOnline mt-8">
-              <Link title="پخش آنلاین" className="text-sm py-2.5 px-5 bg-orange-normal rounded-full"  href="/2">
+              <Link  className="text-sm py-2.5 px-5 bg-orange-normal rounded-full"  href="/2">
                فیلم دو
               </Link>
           </div>

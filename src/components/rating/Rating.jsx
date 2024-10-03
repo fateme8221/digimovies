108002a80a51ed9535d9af92480b9691a08fca3d
fateme <fateme.mhmdi8221@gmail.com>
2024-10-03 11:16:51 +0330

    updat ui

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -4,6 +4,13 @@ function Rating({ post }) {
   return (
     <div className="" >
       <div className="rating-menu flex " >
+         { new Array(5 - post?.rating || 0).fill(0).map((item, index) => (
+             <span className='text-white' key={index}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
+                <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+              </svg>
+            </span>
+          ))}
          { new Array(post?.rating ||0).fill(0).map((item, index) => (
              <span className='text-orange-normal' key={index}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
@@ -11,13 +18,7 @@ function Rating({ post }) {
               </svg>
             </span>
          ))}
-        { new Array(5 - post?.rating || 0).fill(0).map((item, index) => (
-             <span className='text-white' key={index}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
-                <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-              </svg>
-            </span>
-          ))}
+        
       </div>
       <div className="flex text-sm gap-3 mt-2">
         <span className="avg_rate">3.79</span>
